60a96411bb9d50d62a80cd004c43f3337a647bec
himanshu8080 <76471257+himanshu8080@users.noreply.github.com>
2023-05-09 18:10:25 +0530

    checking

--- src/component/Home.js
+++ src/component/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
               <li><a href="#">Gallery</a></li>
               <li><a href="#">Services</a></li>
               <li><a href="#">About Us</a></li>
-              <li><a href="#">Contact Us</a></li>
+              <li><a href="#"> Register</a></li>
             </ul>
           </div>
         </nav>
